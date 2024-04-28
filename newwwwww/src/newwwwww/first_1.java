093b55dd7e98af92105869616f653f2ae4f01f65
Vwivel Fernandes <Vwivel Fernandes@VwivelFernandes>
2024-04-28 06:28:51 +0530

    new first

---
+++ newwwwww/src/newwwwww/first_1.java
@@ -0,0 +1,32 @@
+package newwwwww;
+	import java.util.Scanner;
+
+	public class first_1 {
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.println("Enter the first integer:");
+	        int num1 = scanner.nextInt();
+
+	        System.out.println("Enter the second integer:");
+	        int num2 = scanner.nextInt();
+
+	        int sum = num1 + num2;
+
+	        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+	    }
+		public static int add11(int num1, int num2) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		public static int add(int num1, int num2) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		public static int add1(int num1, int num2) {
+			// TODO Auto-generated method stub
+			return 0;
+		}}
+
+
